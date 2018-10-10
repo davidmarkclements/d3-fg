1414fa7386cf0d99efcfec4637a5c3449234ca1d
Alessio Carnevale <alessio.carnevale@gmail.com>
2018-10-10 22:08:29 +0200

    removing unneeded check of the wrapper.scrollTop value

--- index.js
+++ index.js
@@ -502,7 +502,7 @@ function flameGraph (opts) {
 
     return {
       x: x0,
-      y: transform.applyY(h - frameDepth(node) * c) - wrapper.node().scrollTop,
+      y: transform.applyY(h - frameDepth(node) * c),
       w: x1 - x0,
       h: c
     }
